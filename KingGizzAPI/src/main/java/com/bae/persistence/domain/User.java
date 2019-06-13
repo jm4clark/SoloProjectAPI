ddbb10cee554eba3f9a037a4d5565d4769f17c47
jm4clark <jamesmclark96@gmail.com>
2019-06-13 16:46:06 +0100

    Added base classes for album, song and user

---
+++ KingGizzAPI/src/main/java/com/bae/persistence/domain/User.java
@@ -0,0 +1,34 @@
+package com.bae.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	//private Playlist[] playlists;
+	//private Review[] reviews;
+	
+	public User() {}
+	
+	public User(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+
+}
