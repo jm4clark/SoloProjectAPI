be8da2343a619692ebe2a7f68a01658d77ae38df
jm4clark <jamesmclark96@gmail.com>
2019-06-13 17:38:45 +0100

    fleshed out album database repo

--- KingGizzAPI/src/main/java/com/bae/persistence/repository/AlbumDBRepo.java
+++ KingGizzAPI/src/main/java/com/bae/persistence/repository/AlbumDBRepo.java
@@ -1,35 +1,63 @@
 package com.bae.persistence.repository;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import com.bae.persistence.domain.Album;
+import com.bae.util.JSONUtil;
+
 public class AlbumDBRepo implements AlbumRepository {
+	@PersistenceContext(unitName = "primary")
+	private EntityManager manager;
+
+	@Inject
+	private JSONUtil util;
 
 	@Override
 	public String getAllAlbums() {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = manager.createQuery("Selec a FROM Album a");
+		return util.getJSONForObject(query.getResultList());
 	}
 
 	@Override
 	public String getAnAlbum(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return util.getJSONForObject(manager.find(Album.class, id));
 	}
 
 	@Override
 	public String createAlbum(String album) {
-		// TODO Auto-generated method stub
-		return null;
+		Album a = util.getObjectForJSON(album,  Album.class);
+		manager.persist(a);
+		return util.messageToJSON("album successfully added");
 	}
 
 	@Override
 	public String deleteAlbum(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Album a = util.getObjectForJSON(getAnAlbum(id), Album.class);
+		
+		if(manager.contains(manager.find(Album.class, id))) {
+			manager.remove(manager.find(Album.class, id));
+		}
+		
+		return util.messageToJSON("album successfully deleted");
 	}
 
 	@Override
 	public String updateAlbum(int id, String album) {
-		// TODO Auto-generated method stub
-		return null;
+		Album newAlbum = util.getObjectForJSON(album, Album.class);
+		Album oldAlbum = manager.find(Album.class, id);
+		
+		if(oldAlbum != null) {
+			
+			oldAlbum.setName(newAlbum.getName());
+			//other attributes here
+			
+			manager.persist(oldAlbum);
+		}
+		
+		return util.messageToJSON("album successfully updated");
 	}
 
 }
