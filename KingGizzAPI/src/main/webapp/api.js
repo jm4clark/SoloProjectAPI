51f94fc33b937ecc23995ea93d87d4d0298f030d
jm4clark <jamesmclark96@gmail.com>
2019-06-21 18:37:22 +0100

    url

--- KingGizzAPI/src/main/webapp/api.js
+++ KingGizzAPI/src/main/webapp/api.js
@@ -1,4 +1,4 @@
-const baseUrl =  "/KingGizzAPI/api/"; //http://34.90.83.75:8888/KingGizzAPI/api/"; //  //"http://localhost:8080/KingGizzAPI/api/"; http://35.246.178.250:8888/
+const baseUrl =  "KingGizzAPI/api/"; //http://34.90.83.75:8888/KingGizzAPI/api/"; //  //"http://localhost:8080/KingGizzAPI/api/"; http://35.246.178.250:8888/
 
 function makeRequest(requestType, url, sendData) {
     return new Promise((res, rej) => {
